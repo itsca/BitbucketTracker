1826ea9939718b3daaffeb2d79ab9928a6e02c6a
Javier Escalante <itscae@gmail.com>
2018-01-04 09:48:12 -0600

    Commit made with python and Git hook

--- githubLogger.py
+++ githubLogger.py
@@ -6,11 +6,12 @@ tracker_File_Path = "./tracker.txt"
 contribution_date = str(datetime.datetime.today()).split()[0]
 
 with open(tracker_File_Path, 'a') as the_tracker_file:
+    the_tracker_file.write("\n")    
     the_tracker_file.write(contribution_date)
+    the_tracker_file.write("\n")    
     the_tracker_file.write('Created a private contribution on a bitbucket repository.')
-    the_tracker_file.write("\n")
     the_tracker_file.write("\n")
 
 subprocess.call(["git", "add", "."])
-subprocess.call(["git", "commit", "-m", "Test commit made with python and hook"])
+subprocess.call(["git", "commit", "-m", "Commit made with python and Git hook"])
 subprocess.call(["git", "push", "origin", "master"])
