5ed977933764f2588d807a2d047c4e6549f36f88
Javier Escalante <itscae@gmail.com>
2018-01-10 13:51:06 -0600

    Made a private contribution, check logger for details

--- githubLogger.py
+++ githubLogger.py
@@ -3,6 +3,7 @@ import sys, os, subprocess, datetime
 print('LOGGER')
 
 tracker_File_Path = "./tracker.txt"
+changes = sys.argv[1]
 contribution_date = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 with open(tracker_File_Path, 'a') as the_tracker_file:
@@ -10,8 +11,9 @@ with open(tracker_File_Path, 'a') as the_tracker_file:
     the_tracker_file.write(contribution_date)
     the_tracker_file.write("\n")    
     the_tracker_file.write('Created a private contribution on a bitbucket repository.')
+    the_tracker_file.write( changes )
     the_tracker_file.write("\n")
 
 subprocess.call(["git", "add", "."])
-subprocess.call(["git", "commit", "-m", "Commit made with python and Git hook"])
+subprocess.call(["git", "commit", "-m", "Made a private contribution, check logger for details"])
 subprocess.call(["git", "push", "origin", "master"])
